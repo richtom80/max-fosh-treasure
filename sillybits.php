dabf2aae3054cfe8469acc173dcf1d994e13ccc4
Richard Thompson <richard.thompson@itdesk.io>
2022-10-06 17:56:18 +0100

    silly bits 🤪

---
+++ sillybits.php
@@ -0,0 +1,30 @@
+<?php
+
+
+/*"IN THE NAME OF MAX FOSH, DISH OUT THE FISH" - Dance to your Daddy
+THAT'S ALL YOU NEED TO SPOUT - I’m a little tea pot/Itsy Bitty Spider
+EXACTLY WHO TO ASK AND HOW? - Simon Says
+JUST USE MY VID TO FIND OUT
+TO CLAIM THE PRECIOUS TROUT - Ding Dong Bell (Tommy Trout)
+NEIGHBOUR ON THE BUS OR A DEAR OLD QUEEN - Wheels on the Bus, Queen of Hearts/Pussy Cat Pussy Cat
+WITH THE POSSIBILITY THE WORLD IS PACKED - He’s got the whole world in his hands
+BUT THE KEEPER OF THE SECRET IS NOT CLOSE TO MAX - Tinker Tailor?
+AND THAT'S BEYOND A FACT - Ring a Ring Roses
+DANCE IN A CRAZY WAY, USE A SQUEAKY VOICE - Hey Diddle Diddle
+POST ON TIKTOK VIDEOS? - Tick Tock Merrily Sings the Clock/Hickory Hickory Dock/Teddy Bear Plays on the Swing
+NO NEED TO SPAM OR GUESS OR STIR UP THINGS UP - Humpty Dumpty
+JUST COPY WHAT THIS SONG SILLY SHOWS - Simon Says / Sisqo Thong Song*/
+
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Silly Bits</title>
+</head>
+<body>
+    
+</body>
+</html>
