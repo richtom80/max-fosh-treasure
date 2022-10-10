827de0cec7c530d3165a759d9992b806d1bd4a6e
Richard Thompson <richard.thompson@itdesk.io>
2022-10-10 19:17:20 +0100

    cng

--- sillybits.php
+++ sillybits.php
@@ -25,6 +25,20 @@ JUST COPY WHAT THIS SONG SILLY SHOWS - Simon Says / Sisqo Thong Song*/
     <title>Silly Bits</title>
 </head>
 <body>
+    <h1>Nursery Ryhmes</h1>
+    <pre>"IN THE NAME OF MAX FOSH, DISH OUT THE FISH" - Dance to your Daddy
+THAT'S ALL YOU NEED TO SPOUT - I'm a little tea pot/Itsy Bitty Spider
+EXACTLY WHO TO ASK AND HOW? - Simon Says
+JUST USE MY VID TO FIND OUT
+TO CLAIM THE PRECIOUS TROUT - Ding Dong Bell (Tommy Trout)
+NEIGHBOUR ON THE BUS OR A DEAR OLD QUEEN - Wheels on the Bus, Queen of Hearts/Pussy Cat Pussy Cat
+WITH THE POSSIBILITY THE WORLD IS PACKED - He's got the whole world in his hands
+BUT THE KEEPER OF THE SECRET IS NOT CLOSE TO MAX - Tinker Tailor?
+AND THAT'S BEYOND A FACT - Ring a Ring Roses
+DANCE IN A CRAZY WAY, USE A SQUEAKY VOICE - Hey Diddle Diddle
+POST ON TIKTOK VIDEOS? - Tick Tock Merrily Sings the Clock/Hickory Hickory Dock/Teddy Bear Plays on the Swing
+NO NEED TO SPAM OR GUESS OR STIR UP THINGS UP - Humpty Dumpty
+JUST COPY WHAT THIS SONG SILLY SHOWS - Simon Says / Sisqo Thong Song</pre>
     
 </body>
 </html>
