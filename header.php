17cee15693698df82a1312ac4b476cd417ec8424
Richard Thompson <richard@gwtmedia.co.uk>
2022-10-19 11:40:37 +0100

    header update

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
         <div class="collapse navbar-collapse" id="mfth-navbar">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link <?= ($_SERVER['PHP_SELF'] == "/index.php") ? "active" : ""; ?>" aria-current="page" href="#">W3W Search</a>
+              <a class="nav-link <?= ($_SERVER['PHP_SELF'] == "/index.php") ? "active" : ""; ?>" aria-current="page" href="/">W3W Search</a>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="/wordsearch.php" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">Wordsearches</a>
